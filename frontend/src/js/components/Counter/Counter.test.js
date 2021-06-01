b6a4e814adb731a9c359fdd3b10796a05e4989e5
AntLouiz <luizrodrigo46@hotmail.com>
2021-06-01 14:58:13 -0300

    Fix jest tests

--- frontend/src/js/components/Counter/Counter.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react"
-
-describe("Counter Component", () => {
-    test("should test my initial test", () => {
-        expect(1).toEqual(1)
-    })
-})
