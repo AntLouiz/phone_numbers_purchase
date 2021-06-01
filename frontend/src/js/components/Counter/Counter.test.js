093c8fb7635ea87ec6a70b75c1a314fa4d4346cd
AntLouiz <luizrodrigo46@hotmail.com>
2021-06-01 14:14:46 -0300

    Add jest and redux

---
+++ frontend/src/js/components/Counter/Counter.test.js
@@ -0,0 +1,7 @@
+import { render, screen } from "@testing-library/react"
+
+describe("Counter Component", () => {
+    test("should test my initial test", () => {
+        expect(1).toEqual(1)
+    })
+})
