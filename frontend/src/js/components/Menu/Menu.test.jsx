9d332739cf3b73cd32b22c4c011b1ea65dca76e2
AntLouiz <luizrodrigo46@hotmail.com>
2021-06-02 18:17:23 -0300

    Add more tests to Menu component

--- frontend/src/js/components/Menu/Menu.test.jsx
+++ frontend/src/js/components/Menu/Menu.test.jsx
@@ -12,9 +12,31 @@ const render = (ui) => {
 }
 
 describe("Menu Component", () => {
-    test("should assert the menu links texts", () => {
+    test("should assert the menu links", () => {
       render(<Menu />)
-      let menuElement = screen.getByText("0")
-      expect(menuElement).toBeInTheDocument()
+      const indexLink = screen.getByText("PhoneNumbers")
+      const purchaseLink = screen.getByText("Purchase Phones")
+      const myNumbersLink = screen.getByText("My Numbers")
+
+      expect(indexLink.href).toContain('#')
+      expect(purchaseLink.href).toContain('#purchase')
+      expect(myNumbersLink.href).toContain('#my-numbers')
     })
+
+    test("should have a search component", () => {
+      render(<Menu />)
+      const searchElement = screen.getByPlaceholderText("Search phone numbers")
+      expect(searchElement).toBeInTheDocument()
+      expect(searchElement.type).toBe('search')
+
+      const searchButtonElement = screen.getByText("search")
+      expect(searchButtonElement).toBeInTheDocument()
+    })
+
+    test("should have a .navbar class", () => {
+      const { container } = render(<Menu />)
+      expect(container.firstChild).toHaveClass('navbar')
+    })
+
+    // test("should have ")
 })
