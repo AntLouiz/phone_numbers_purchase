43d8cf1387074564374007c30ffd1058bd2f0a12
AntLouiz <luizrodrigo46@hotmail.com>
2021-06-03 18:16:15 -0300

    Fix value max_length

--- backend/phones/models.py
+++ backend/phones/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Phone(models.Model):
-    value = models.CharField(max_length=10)
+    value = models.CharField(max_length=50)
     monthy_price = models.IntegerField(default=1)
     currency = models.CharField(max_length=10)
     setup_price = models.IntegerField(default=1)
