c6c14473953521b72f9a473d9ef3e574aa0d0400
AntLouiz <luizrodrigo46@hotmail.com>
2021-06-03 18:26:57 -0300

    Move getMockedPhones function to mocks folder

---
+++ frontend/src/__mocks__/phonesMock.js
@@ -0,0 +1,18 @@
+function getMockedPhones () {
+  let phones = []
+  console.log("BUSCANDO O MOCK")
+  for (let index = 1; index <= 800; index++) {
+    let phone = {
+      "id": index,
+      "value": `+55 84 91234-432${index}`,
+      "monthyPrice": `0.1${index}`,
+      "setupPrice": `${index}.40`,
+      "currency": "BRL$"
+    }
+    phones.push(phone)
+  }
+
+  return phones
+}
+
+export default getMockedPhones
--- frontend/src/js/ducks/phoneListSlice.js
+++ frontend/src/js/ducks/phoneListSlice.js
@@ -1,21 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
-
-
-function getMockedPhones () {
-  let phones = []
-  for (let index = 1; index <= 800; index++) {
-    let phone = {
-      "id": index,
-      "value": `+55 84 91234-432${index}`,
-      "monthyPrice": `0.1${index}`,
-      "setupPrice": `${index}.40`,
-      "currency": "BRL$"
-    }
-    phones.push(phone)
-  }
-
-  return phones
-}
+import getMockedPhones from '../../__mocks__/phonesMock'
 
 export const phoneListSlice = createSlice({
     name: 'phones',
