d1a7d8fb03ac0248470b11f2b7db7db317395305
AntLouiz <luizrodrigo46@hotmail.com>
2021-06-04 11:55:49 -0300

    Remove counter component

--- frontend/src/js/app/store.js
+++ frontend/src/js/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../ducks/counterSlice'
 import phoneListSlice from '../ducks/phoneListSlice'
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     phones: phoneListSlice
   },
 })
