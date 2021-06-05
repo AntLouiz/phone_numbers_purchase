7123499631208da907ed4deb23a8f38e7087754c
AntLouiz <luizrodrigo46@hotmail.com>
2021-06-05 12:16:27 -0300

    Add purchasePhone api call

--- frontend/src/js/api/phones.js
+++ frontend/src/js/api/phones.js
@@ -1,4 +1,4 @@
-function getPhones (payload, handleSuccess, handleError) {
+export function getPhones (payload, handleSuccess, handleError) {
   const { pageIndex } = payload
   let url = `/api/phones/?page=${pageIndex}`
 
@@ -11,4 +11,23 @@ function getPhones (payload, handleSuccess, handleError) {
   }).catch((error) => handleError(error))
 }
 
-export default getPhones
+export function purchasePhone (payload, handleSuccess, handleError) {
+  const { id } = payload
+  let url = `/api/phones/${id}/`
+  const requestOptions = {
+    method: 'PATCH',
+    body: JSON.stringify({'isPurchased': true, 'id': id}),
+    headers: new Headers({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    })
+  };
+
+  let request = fetch(url, requestOptions)
+
+  request.then((response) => {
+    return response.json()
+  }).then((data) => {
+    handleSuccess(data)
+  }).catch((error) => handleError(error))
+}
